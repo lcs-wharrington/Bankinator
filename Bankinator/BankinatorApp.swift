5f3b284101341c3090ce2b35a4c1d53505e94657
William Harrington <wharrington@lcs.on.ca>
2022-05-06 11:25:02 -0400

    Initial Commit

---
+++ Bankinator/BankinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankinatorApp.swift
+//  Bankinator
+//
+//  Created by William Robert Harrington on 2022-05-06.
+//
+
+import SwiftUI
+
+@main
+struct BankinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bankinator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bankinator
+//
+//  Created by William Robert Harrington on 2022-05-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
